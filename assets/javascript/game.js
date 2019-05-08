245e7ce7b71cac742ee6c87f681f2e9b4b3531f6
zwarnerduncan <zwarnerduncan@yahoo.com>
2019-05-08 13:53:53 -0400

    added guess from player and part of the if else condition for the guessing state

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,3 +20,19 @@ for (var i = 0; i < fashionshirts.length; i++) {
 }
 
 var remainingletters = fashionshirts.length;
+
+while (remainingletters > 0) {
+    alert (fashionanswers.join (" "));
+}
+
+//Guess from player
+var Guess = prompt("Guess a letter to play");
+    if (guess === null) {
+        // This is to exit
+        break;
+    } else if (guess.length !== 1) {
+        alert ("PLease enter onlt one letter.");
+
+    } else {
+        for (var j)
+    }
